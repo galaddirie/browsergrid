d7df92a7d1bedd7f09a85bf1ff23383efb5f91e9
Galad Dirie <galad.work@gmail.com>
2025-09-28 20:00:37 -0400

    Remove old browser Dockerfiles and related scripts

--- docker/browsers/base/nginx/setup_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Test nginx configuration
-nginx -t
-
-# Nginx configuration is now static and properly configured
-echo "Nginx configured to proxy to browsermux on port 8080" 
