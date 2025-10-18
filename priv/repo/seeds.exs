ccb8c11baff1b957e87bd471f0a90689d8d4622b
Galad Dirie <galad.work@gmail.com>
2025-10-18 04:32:49 -0400

    Update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,31 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Import required modules
+alias Browsergrid.Repo
+alias Browsergrid.Accounts
+alias Browsergrid.Accounts.User
+
+# Seed a non-admin user
+IO.puts("Creating non-admin user...")
+
+case Accounts.get_user_by_email("user@example.com") do
+  nil ->
+    # User doesn't exist, create them
+    {:ok, user} = Accounts.register_user(%{
+      email: "user@example.com",
+      password: "password123456"
+    })
+
+    # Confirm the user account
+    user
+    |> User.confirm_changeset()
+    |> Repo.update!()
+
+    IO.puts("Non-admin user created and confirmed: #{user.email}")
+
+  existing_user ->
+    # User already exists
+    IO.puts("Non-admin user already exists: #{existing_user.email}")
+end
